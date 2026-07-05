b706066815e64db900f93b420ad3ba60eb493ddb
Diorgenes George <DgtsSK8@hotmail.com>
2026-07-05 10:03:12 -0300

    feat: adicionar componente PapelRasgado

---
+++ src/componentes/PapelRasgado.tsx
@@ -0,0 +1,16 @@
+export function PapelRasgado() {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      viewBox="0 0 400 40"
+      preserveAspectRatio="none"
+      className="block w-full h-10 text-branco-papel"
+    >
+      <path
+        d="M0,20 L20,8 L40,24 L60,4 L80,18 L100,10 L120,26 L140,6 L160,20 L180,12 L200,22 L220,8 L240,18 L260,4 L280,24 L300,10 L320,20 L340,6 L360,22 L380,12 L400,20 L400,40 L0,40 Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
